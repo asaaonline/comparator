10448873f1e046fba7d0ed11cde8f0a173d12b47
asela <ecomsearchonline@gmail.com>
2020-01-30 00:18:11 +0530

    first commit

---
+++ src/main/java/lk/av/ruh/eng/mobilepricecompair/MobilePriceCompairApplication.java
@@ -0,0 +1,13 @@
+package lk.av.ruh.eng.mobilepricecompair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePriceCompairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilePriceCompairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/av/ruh/eng/mobilepricecompair/MobilePriceCompairApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.av.ruh.eng.mobilepricecompair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePriceCompairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
