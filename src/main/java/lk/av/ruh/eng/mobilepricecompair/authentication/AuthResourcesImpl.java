1d19869a71d0df4a1fc76bf1b82dc39d29c788b7
asela <ecomsearchonline@gmail.com>
2020-02-01 22:54:41 +0530

    change mysql database settings

--- src/main/java/lk/av/ruh/eng/mobilepricecompair/authentication/AuthResourcesImpl.java
+++ src/main/java/lk/av/ruh/eng/mobilepricecompair/authentication/AuthResourcesImpl.java
@@ -1,7 +1,12 @@
 package lk.av.ruh.eng.mobilepricecompair.authentication;
 
+import lk.av.ruh.eng.mobilepricecompair.commonModels.Response;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthResourcesImpl implements AuthResources {
+    @Override
+    public Response login(String userName, String password) {
+        return null;
+    }
 }
