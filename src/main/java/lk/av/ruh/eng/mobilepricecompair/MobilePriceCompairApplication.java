6c1f0cf5f50ebcb705181058757ee7a17f8bb999
asela <ecomsearchonline@gmail.com>
2020-02-01 23:45:35 +0530

    web secutity added to pom

--- src/main/java/lk/av/ruh/eng/mobilepricecompair/MobilePriceCompairApplication.java
+++ src/main/java/lk/av/ruh/eng/mobilepricecompair/MobilePriceCompairApplication.java
@@ -2,6 +2,9 @@ package lk.av.ruh.eng.mobilepricecompair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MobilePriceCompairApplication {
@@ -10,4 +13,8 @@ public class MobilePriceCompairApplication {
         SpringApplication.run(MobilePriceCompairApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
