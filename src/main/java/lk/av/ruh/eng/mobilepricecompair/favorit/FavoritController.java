276abbccdcabc9235d695be8694efe6c9a00c425
asela <ecomsearchonline@gmail.com>
2020-02-15 16:40:48 +0530

    create favorite controller

---
+++ src/main/java/lk/av/ruh/eng/mobilepricecompair/favorit/FavoritController.java
@@ -0,0 +1,31 @@
+package lk.av.ruh.eng.mobilepricecompair.favorit;
+
+import lk.av.ruh.eng.mobilepricecompair.commonModels.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController("favorit")
+public class FavoritController {
+
+    @PutMapping("add/{uId}")
+    public ResponseEntity<Response> makeFavorite(
+            @PathVariable String uId,
+            @RequestParam Long phoneId
+    ) {
+        return null;
+    }
+
+    @PutMapping("remove/{uId}")
+    public ResponseEntity<Response> removeFavorite(
+            @PathVariable String uId,
+            @RequestParam Long phoneId
+    ) {
+        return null;
+    }
+
+}
